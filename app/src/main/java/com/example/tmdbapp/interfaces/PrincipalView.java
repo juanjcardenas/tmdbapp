ce1a83fa098ff4fa68321ed62529fe8e9fd8c4a9
juanjcardenas <juanchoct123@gmail.com>
2021-09-13 11:56:46 -0500

    eliminamos metodos y especios

--- app/src/main/java/com/example/tmdbapp/interfaces/PrincipalView.java
+++ app/src/main/java/com/example/tmdbapp/interfaces/PrincipalView.java
@@ -1,10 +1,8 @@
 package com.example.tmdbapp.interfaces;
-import retrofit2.Call;
-import retrofit2.http.GET;
+
 
 public interface PrincipalView {
-    @GET("day?api_key=cdaef7b595abd109ff25f3328c6badff")
-    Call<PeliculaRespuesta> getListaPeliculas();
+
 
 
 }
